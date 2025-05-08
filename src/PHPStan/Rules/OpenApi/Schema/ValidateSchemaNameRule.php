dc7237d7e70fb31c1d06f66847b07dd256ef30ea
Algirdas Čičinskas <algirdas.cicinskas@hostinger.com>
2025-05-08 10:04:18 +0300

    schema name rules

---
+++ src/PHPStan/Rules/OpenApi/Schema/ValidateSchemaNameRule.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenApiTools\PHPStan\Rules\OpenApi\Schema;
+
+use OpenApi\Attributes\Schema;
+use OpenApiTools\PHPStan\Helpers\Attributes;
+use OpenApiTools\PHPStan\Helpers\RuleIdentifier;
+use PhpParser\Node;
+use PhpParser\Node\Stmt;
+use PHPStan\Analyser\Scope;
+use PHPStan\DependencyInjection\Container;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use PHPStan\ShouldNotHappenException;
+
+class ValidateSchemaNameRule implements Rule
+{
+    public function __construct(
+        private readonly ReflectionProvider $reflectionProvider,
+    ) {
+    }
+
+    public function getNodeType(): string
+    {
+        return Stmt\Class_::class;
+    }
+
+    /**
+     * @throws ShouldNotHappenException
+     */
+    public function processNode(Node $node, Scope $scope): array
+    {
+        if (!$node instanceof Stmt\Class_) {
+            return [];
+        }
+
+        $className = (string) $node->namespacedName;
+
+        $reflectionClass = $this->reflectionProvider->getClass($className)->getNativeReflection();
+        $classSchema = Attributes::getAttributes($reflectionClass, Schema::class)[0]->newInstance() ?? null;
+
+        if ($classSchema === null) {
+            return [];
+        }
+
+        $preferredSchemaName = str_replace('\\', '.', str_replace('App\\Http\\', '', (string) $node->namespacedName));
+
+        return [
+            RuleErrorBuilder::message(sprintf('Schema name "%s" does not match "%s"', $classSchema->schema, $preferredSchemaName))
+                ->identifier(RuleIdentifier::identifier('schemaNameDoesNotMatchNamespace'))
+                ->build()
+        ];
+    }
+}
